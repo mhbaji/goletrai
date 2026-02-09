e73f410a8f6ceca2dfa261422c8801d154576bed
mhbaji <mhilalbayuaji@gmail.com>
2026-02-09 22:26:28 +0700

    feat: base detector module

---
+++ goletrai/__init__.py
@@ -0,0 +1,58 @@
+import cv2
+import numpy as np
+import onnxruntime as ort
+import os 
+
+cFile = os.path.abspath(__file__)
+basepath = os.path.dirname(cFile)
+MODELPATH = os.path.join(basepath, "src/rai.onnx")
+IMAGEPATH = os.path.join(basepath, "src/rai.jpg")
+
+class GoletRai:
+    def __init__(self, model_path:str=MODELPATH, iou_thresh:float=0.5):
+        self.session = ort.InferenceSession(model_path, providers=["CPUExecutionProvider"])
+        self.model_inputs = self.session.get_inputs()
+        input_shape = self.model_inputs[0].shape
+        self.input_height = input_shape[2]
+        self.input_width = input_shape[3]
+        self.iou_thresh = iou_thresh
+
+    def preprocess(self, image):
+        img = cv2.resize(image, (self.input_width, self.input_height))
+        img = np.array(img) / 255.0
+        img = np.transpose(img, (2, 0, 1))
+        img = img.astype(np.float32)
+        img = np.expand_dims(img, axis=0)
+        return img
+
+    def ning(self, image, conf_thresh:float=0.5):
+        image_input = self.preprocess(image)
+        outputs = self.session.run(None, {self.model_inputs[0].name: image_input})
+        bboxes = []
+        scores = []
+        for outt in outputs[0][0]:
+            x1, y1, x2, y2, confs, clsss = outt
+            if confs >= conf_thresh:
+                bbx = [int(_bbx) for _bbx in [x1, y1, x2, y2]]
+                bboxes.append(bbx)
+                scores.append(float(confs))
+
+        indices = cv2.dnn.NMSBoxes(bboxes, scores, conf_thresh, self.iou_thresh)
+        results = []
+        for i in indices.flatten():
+            xmin, ymin, xmax, ymax = bboxes[i]
+            results.append([xmin, ymin, xmax, ymax, scores[i]])
+        return results
+    
+    def coba(self, conf_thresh:float=0.5, show:bool=False):
+        image = cv2.imread(IMAGEPATH)
+        results = self.ning(image, conf_thresh)
+        for result in results:
+            print(f"BBOX: {result[:4]} - CONF: {result[-1]}")
+            if not show: continue
+            cv2.rectangle(image, (result[0], result[1]), (result[2], result[3]), (0, 0, 255), 2, 1)
+
+        if show:
+            cv2.imshow('image', image)
+            cv2.waitKey()
+            cv2.destroyAllWindows()
---
+++ main.py
@@ -0,0 +1,6 @@
+from goletrai import GoletRai as Grai
+
+if __name__ == '__main__':
+    grai = Grai()
+    grai.coba(show=True)
+    pass
